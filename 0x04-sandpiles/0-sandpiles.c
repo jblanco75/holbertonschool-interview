b3c1899a4a445399b79ee074df31ab46435cda85
jblanco75 <jblanco75@gmail.com>
2022-01-16 15:44:24 -0500

    Sandpiles sum

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,54 +1,5 @@
 #include "sandpiles.h"
 /**
- * sandpiles_sum - Stablish the sandpile result of a sum of two sandpiles.
- *
- * @grid1: First Sandpile.
- * @grid2: Second Sandpile.
- * Return: Nothing.
- */
-void sandpiles_sum(int grid1[3][3], int grid2[3][3])
-{
-	int n = 0, i, j, new_grid[3][3];
-
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			grid1[i][j] += grid2[i][j];
-			if (grid1[i][j] > 3)
-				n++;
-		}
-	}
-	while (n != 0)
-	{
-		printf("=\n");
-		print_grid(grid1);
-		for (i = 0; i < 3; i++)
-			for (j = 0; j < 3; j++)
-				new_grid[i][j] = grid1[i][j];
-		for (i = 0; i < 3; i++)
-		{
-			for (j = 0; j < 3; j++)
-			{
-				if (new_grid[i][j] > 3)
-				{
-					grid1[i][j] -= 4;
-					(i - 1) >= 0 ? grid2[i - 1][j] += 1 : 0;
-					(i + 1) <= 2 ? grid2[i + 1][j] += 1 : 0;
-					(j - 1) >= 0 ? grid2[i][j - 1] += 1 : 0;
-					(j + 1) <= 2 ? grid2[i][j + 1] += 1 : 0;
-				}
-			}
-		}
-		n = 0;
-		for (i = 0; i < 3; i++)
-			for (j = 0; j < 3; j++)
-				if (grid1[i][j] > 3)
-					n++;
-	}
-}
-
-/**
  * print_grid - Print 3x3 grid
  * @grid: 3x3 grid
  *
@@ -66,5 +17,107 @@ static void print_grid(int grid[3][3])
 			printf("%d", grid[i][j]);
 		}
 		printf("\n");
+	}
+}
+
+/**
+ * grid_copy - copy a grid in another one
+ * @grid1: first sandpile
+ * @grid2: second sandpile
+ *
+ * Return: nothing
+ */
+void grid_copy(int grid2[3][3], int grid1[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] = grid2[i][j];
+		}
+	}
+}
+/**
+ * not_stable - verify if the grid is unstable
+ * @grid1: sandpile
+ *
+ * Return: 1 if it is unstable or 0 if not
+ */
+int not_stable(int grid1[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] > 3)
+				return (1);
+		}
+	}
+	return (0);
+}
+/**
+ * zero_filler - fill a grid with zeros
+ * @grid2: sandpile
+ *
+ * Return: nothing
+ */
+void zero_filler(int grid2[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid2[i][j] = 0;
+		}
+	}
+}
+/**
+ * sandpiles_sum - computes the sum of two sandpiles
+ * @grid1: first sandpile
+ * @grid2: second sandpile
+ *
+ * Return: nothing
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+			grid1[i][j] = grid1[i][j] + grid2[i][j];
+	}
+	while (not_stable(grid1))
+	{
+		printf("=\n");
+		print_grid(grid1);
+		zero_filler(grid2);
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				if (grid1[i][j] > 3)
+				{
+					grid2[i][j] += grid1[i][j] - 4;
+					if (i - 1 >= 0)
+						grid2[i - 1][j] += 1;
+					if (j - 1 >= 0)
+						grid2[i][j - 1] += 1;
+					if (i + 1 <= 2)
+						grid2[i + 1][j] += 1;
+					if (j + 1 <= 2)
+						grid2[i][j + 1] += 1;
+				}
+				else
+					grid2[i][j] += grid1[i][j];
+			}
+		}
+		grid_copy(grid2, grid1);
 	}
 }
