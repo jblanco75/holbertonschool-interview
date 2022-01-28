a15602dcd7f1a6b558565fddbdbd68c6f01dfb34
jblanco75 <jblanco75@gmail.com>
2022-01-27 23:40:42 -0500

    Log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -7,35 +7,41 @@ Script that reads stdin line by line and computes metrics
 from sys import stdin
 
 
-def statPrinter(fileSize, codeBook):
+def print_stats(file_size, status_codes):
     """Prints file size and codes"""
-    print("File size: {}".format(fileSize))
-    for code in sorted(codeBook.keys()):
-        if codeBook[code] != 0:
-            print("{}: {}".format(code, codeBook[code]))
+    print("File size: {}".format(file_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] != 0:
+            print("{}: {}".format(code, status_codes[code]))
 
 
-lineCount = fileSize = currentCode = 0
-codeBook = {
-    '200': 0, '301': 0, '400': 0, '401': 0,
-    '403': 0, '404': 0, '405': 0, '500': 0
+count = file_size = currentCode = 0
+status_codes = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
 }
 
 try:
     for line in stdin:
-        lineCount += 1
+        count += 1
         currentLine = line.split()
 
         if currentLine[-1].isnumeric():
-            fileSize += int(currentLine[-1])
+            file_size += int(currentLine[-1])
 
-        if len(currentLine) > 2 and currentLine[-2] in codeBook:
-            codeBook[currentLine[-2]] += 1
+        if len(currentLine) > 2 and currentLine[-2] in status_codes:
+            status_codes[currentLine[-2]] += 1
 
-        if lineCount % 10 == 0:
-            statPrinter(fileSize, codeBook)
+        if count % 10 == 0:
+            print_stats(file_size, status_codes)
 
-    statPrinter(fileSize, codeBook)
+    print_stats(file_size, status_codes)
 
 except (KeyboardInterrupt):
-    statPrinter(fileSize, codeBook)
+    print_stats(file_size, status_codes)
