a972b96b2e415a03582447f10f6acbb63cab1686
jblanco75 <jblanco75@gmail.com>
2022-01-27 23:22:42 -0500

    Log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -7,52 +7,35 @@ Script that reads stdin line by line and computes metrics
 from sys import stdin
 
 
-codes = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0
+def statPrinter(fileSize, codeBook):
+    """Prints file size and codes"""
+    print("File size: {}".format(fileSize))
+    for code in sorted(codeBook.keys()):
+        if codeBook[code] != 0:
+            print("{}: {}".format(code, codeBook[code]))
+
+
+lineCount = fileSize = currentCode = 0
+codeBook = {
+    '200': 0, '301': 0, '400': 0, '401': 0,
+    '403': 0, '404': 0, '405': 0, '500': 0
 }
 
-size = 0
-i = 0
+try:
+    for line in stdin:
+        lineCount += 1
+        currentLine = line.split()
 
+        if currentLine[-1].isnumeric():
+            fileSize += int(currentLine[-1])
 
-def statprinter():
-    """
-    Prints accumulated logs
-    """
-    print('File size: {}'.format(int(size)))
-    for status_code, value in codes.items():
-        if value == 0:
-            continue
+        if len(currentLine) > 2 and currentLine[-2] in codeBook:
+            codeBook[currentLine[-2]] += 1
 
-        print('{}: {}'.format(status_code, value))
+        if lineCount % 10 == 0:
+            statPrinter(fileSize, codeBook)
 
+    statPrinter(fileSize, codeBook)
 
-if __name__ == "__main__":
-
-    try:
-        while True:
-            current_line = stdin.readline().strip()
-            tokens = current_line.split(' ')
-
-            if len(tokens) != 9:
-                pass
-
-            current_size, status_code = tokens[-1], tokens[-2]
-            size += float(current_size)
-            codes[status_code] += 1
-            i += 1
-
-            if i == 10:
-                statprinter()
-                i = 0
-    except KeyboardInterrupt:
-        statprinter()
-        raise
-    statprinter()
+except (KeyboardInterrupt):
+    statPrinter(fileSize, codeBook)
