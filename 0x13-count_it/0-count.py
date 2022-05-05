3db0bdd8cccce7ca0c3033be9909d523bf35fcb6
jblanco75 <jblanco75@gmail.com>
2022-05-05 17:50:57 -0500

    Count it!

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -4,52 +4,39 @@
 import requests
 
 
-def print_greater_than_zero(word_counter):
-    """
-    print all keys and values grater than zero
-    """
-    keys = list(word_counter.keys())
-
-    keys.sort()
-
-    for k in keys:
-        v = word_counter[k]
-        if v > 0:
-            print("{}: {}".format(k, v))
-
-
-def count_words(subreddit, word_list, after="", word_counter={}):
-    """
-    queries the Reddit API,
-    """
-    if after == "":
-        for i in range(len(word_list)):
-            word_list[i] = word_list[i].lower()
-            word_counter[word_list[i]] = 0
-
-    if after is None:
-        print_greater_than_zero(word_counter)
+def get_articles(sub, w, wd, wd_r, a="",):
+    """ Get articles """
+    url = "https://www.reddit.com/r/{}".format(sub)
+    url = "{}/hot.json?limit=100&after={}".format(url, a)
+    response = requests.get(url, allow_redirects=False,
+                            headers={'User-agent': 'Holberton'})
+    if response.status_code != 200:
+        return None
+    articles = response.json()["data"]["children"]
+    for article in articles:
+        titles = article["data"]["title"].lower().split(" ")
+        for title in titles:
+            if title in wd:
+                wd[title] += 1
+    if response.json()["data"]["after"] is None:
+        sw = sorted(wd.items(), key=lambda t: t[::-1])
+        sw_desc = sorted(sw, key=lambda tup: tup[1], reverse=True)
+        for w in sw_desc:
+            if w[1] > 0:
+                print("{}: {}".format(w[0], w[1] * wd_r[w[0]]))
         return
+    return get_articles(sub, w, wd, wd_r, response.json()["data"]["after"])
 
-    headers = {"user-agent": "myapp/v0.1"}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
 
-    try:
-        res = requests.get(url, headers=headers)
-
-        json_data = res.json().get("data")
-        after = json_data.get("after")
-
-        childrens = json_data.get("children")
-
-        for children in childrens:
-            title = children.get("data").get("title").split()
-
-            for j in title:
-                j = j.lower()
-                if j in word_counter.keys():
-                    word_counter[j] += 1
-
-        count_words(subreddit, word_list, after, word_counter)
-    except Exception:
-        return
+def count_words(subreddit, word_list):
+    """ Count words """
+    w_d = {}
+    w_d_r = {}
+    word_list = [word.lower() for word in word_list]
+    for w in word_list:
+        if w not in w_d:
+            w_d_r[w] = 1
+            w_d[w] = 0
+        else:
+            w_d_r[w] += 1
+    get_articles(subreddit, word_list, w_d, w_d_r)
