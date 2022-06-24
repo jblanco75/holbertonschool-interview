19556d1be9ee2e03d001fb974653822f921ad018
jblanco75 <jblanco75@gmail.com>
2022-06-23 21:22:36 -0500

    Change comes from within

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -8,15 +8,11 @@ def makeChange(coins, total):
     """Making change method"""
     if total <= 0:
         return 0
-
     if type(coins) is not list:
         return
-
     if coins == []:
         return -1
-
     coins.sort(reverse=True)
-
     len_coins = len(coins)
     i = 0
     counter = 1
